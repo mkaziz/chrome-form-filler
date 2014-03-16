c9198e57c54b0086a570edc5ec4b81bce2bda6fa
Khalid <kaziz@u.northwestern.edu>
2014-03-15 22:13:58 -0500

    Added model for field

---
+++ js/app/models/field.js
@@ -0,0 +1,22 @@
+define([
+  'app/base/model'
+], function (BaseModel) {
+
+  BaseModel.extend({
+    defaults: {
+      cssSelector: '',
+      value: '',
+      override: false,
+      type: 'text'
+    },
+    
+    initialize: function(opts) {
+      if (!!opts) {
+        this.cssSelector = opts.cssSelector;
+        this.value = opts.value;
+        this.override = opts.override;
+        this.type=opts.type;
+      }
+    }
+  });
+
